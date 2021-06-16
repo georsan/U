9830696dbdc6892c852069724619c37760e89a2b
Georsan <jorgesandoval529@gmail.com>
2021-06-16 16:33:40 -0500

    Analisis

---
+++ Analisis/Lab9_Jorge_Sandoval/my_simpson_function_Sandoval_Jorge.m
@@ -0,0 +1,12 @@
+function my_simpson_function_Sandoval_Jorge(fun,a,b,M )
+    h=(b-a)/M;
+    s=fun(a)+fun(b);
+    for i=1:2:M-1
+        s=s+4*fun(a+i*h);
+    end
+    for i=2:2:M-2
+        s=s+2*fun(a+i*h);
+    end
+    I=h/3*s;
+    disp(I)
+end
