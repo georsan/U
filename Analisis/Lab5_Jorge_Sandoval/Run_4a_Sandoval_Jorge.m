43fb0e1b09115b4c6ea2a4673727da96ed83941e
Georsan <jorgesandoval529@gmail.com>
2021-06-18 14:41:07 -0500

    Terminado

--- Analisis/Lab5_Jorge_Sandoval/Run_4a_Sandoval_Jorge.m
+++ Analisis/Lab5_Jorge_Sandoval/Run_4a_Sandoval_Jorge.m
@@ -1,9 +1,4 @@
-%Ecuación
-%---------------
-%10P+15M+40G=300
-%P-M-G=0
-%M-2G=0
-%---------------
+
 
 %Script
 A=[10 15 40;1 -1 -1;0 1 -2];
@@ -12,4 +7,4 @@ b=[300;0;0];
 X=U\(L\b);
 X
 
-%Sophia necesita vender 9 pequeñas , 6 medianas y 3 grandes.
+
