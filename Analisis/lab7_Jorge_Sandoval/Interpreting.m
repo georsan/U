42ee13b3034af428441b3d3f53cef15072ae20df
Georsan <jorgesandoval529@gmail.com>
2021-06-21 14:52:01 -0500

    Terminado

---
+++ Analisis/lab7_Jorge_Sandoval/Interpreting.m
@@ -0,0 +1,20 @@
+Y = [87.97 224.70 365.26 686.98 4332.59 10759.22 30685.40 60189].*3600; 
+X = [57.91 108.70 149.60 227.92 778.57 1433.53 2872.46 4495.06].*10^6;
+n=size(Y,2);
+ for i=1:n
+ X(i)=log10(X(i).^3);
+ Y(i)=log10(Y(i).^2);
+ end
+ 
+[A,B]=my_isline_Sandoval_Jorge(X,Y);
+
+%%%%%%%%%%%%%%%%
+
+C=4*(pi)^2/((6.674*10^-11)*(1.989*10^30));
+c=10^B;
+%error absoluto
+Er=abs(C-c);
+%error relativo
+Ea=Er/C;
+disp('el error absoluto es ');
+disp(Ea)
--- Analisis/lab7_Jorge_Sandoval/my_isline_Sandoval_Jorge.m
+++ Analisis/lab7_Jorge_Sandoval/my_isline_Sandoval_Jorge.m
@@ -1,14 +1,15 @@
 function [A,B]=my_isline_Sandoval_Jorge(X,Y)
 X = X(:);
 Y = Y(:);
+xm = mean(X);
+ym = mean(Y);
+n=size(X,1);
+numer=0;
+d=0;
+Ex=0;
+Ey=0;
 if  size(X,1)==size(Y,1)
-    xm = mean(X);
-    ym = mean(Y);
-    n=size(X,1);
-    numer=0;
-    d=0;
-    Ex=0;
-    Ey=0;  
+      
     for i=1:n
         numer = numer + (X(i,1)-xm)*(Y(i,1)-ym);
         d = d + ((X(i,1)-xm)^2);
@@ -18,12 +19,14 @@ if  size(X,1)==size(Y,1)
 XD = numer/d;
 B = (Ey-(XD*Ex))/n;
 
+
 A = num2str(XD);
 b = num2str(B);
-if b>=0
+
+if B>=0
     fun = strcat('f(x)= ',A,'*X + ',b);
 else
-    fun = strcat('f(x)= ',A,' *X  ',b);
+    fun = strcat('f(x)= ',A,'*X ',b);
 end
 x1 = min(X)-1:0.1:max(X)+1;
 z = XD*x1 + B;
